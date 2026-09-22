632c0a4ed8c9af6f8e7e592da34522ccfb7adf19
Keyla <keylaespinosaramos336@gmail.com>
2026-09-22 15:50:21 -0600

    Forzar firma debug en APK y extraer SHA1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,9 +27,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Asigna la firma debug para que el APK release salga firmado siempre
+            signingConfig signingConfigs.debug
         }
     }
 }
